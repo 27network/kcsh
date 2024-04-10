0f734a5f5f4dc3dd75c953762215f32be1709146
killian <oss@xtrm.me>
2024-04-10 19:52:08 +0200

    🔨 chore(norm): fix

--- submodules/cli/src/runner/msh_runner_error_handler.c
+++ submodules/cli/src/runner/msh_runner_error_handler.c
@@ -6,7 +6,7 @@
 /*   By: kiroussa <oss@xtrm.me>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 18:51:02 by kiroussa          #+#    #+#             */
-/*   Updated: 2024/04/10 19:20:57 by kiroussa         ###   ########.fr       */
+/*   Updated: 2024/04/10 19:51:52 by kiroussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@
 void	msh_runner_error_handler(t_minishell *msh, const char *msg)
 {
 	ft_putstr_fd(STDERR_FILENO, msh->name);
-	ft_putstr_fd(STDERR_FILENO, ": line 1");
-	// ft_putstr_fd(STDERR_FILENO, ft_itoa(msh->script_ctx->line));
+	ft_putstr_fd(STDERR_FILENO, ": line ");
+	ft_putstr_fd(STDERR_FILENO, "N/A");
 	ft_putstr_fd(STDERR_FILENO, ": ");
 	ft_putstr_fd(STDERR_FILENO, msg);
 }
