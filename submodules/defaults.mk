7af3fb4821144c128a48ccd57740d30aaf43d420
killian <oss@xtrm.me>
2024-09-13 19:10:21 +0200

    🔨 chore: remove c23 compilation flag
    Signed-off-by: killian <oss@xtrm.me>

--- submodules/defaults.mk
+++ submodules/defaults.mk
@@ -1,23 +1,23 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    defaults.mk                                        :+:      :+:    :+:    #
+#    config.mk                                          :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: kiroussa <oss@xtrm.me>                     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/02/16 05:36:04 by kiroussa          #+#    #+#              #
-#    Updated: 2024/09/11 23:39:57 by kiroussa         ###   ########.fr        #
+#    Updated: 2024/09/13 19:09:28 by kiroussa         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 PROJECT_NAME	?= kcsh
-PROJECT_VERSION	?= 0.5.2
+PROJECT_VERSION	?= 0.5.5
 
 SRC_DIR			?= src
 INCLUDE_DIR		?= include
 
 CC				?= clang
-CFLAGS			+= -Wall -Wextra -Werror -g3 -fno-plt -fPIE -std=c23
+CFLAGS			+= -Wall -Wextra -Werror -g3 -fno-plt -fPIE
 DFLAGS 			+= -MT $@ -MMD -MP -MF $(D_DIR)/$*.tmp.d
 
 ifeq ($(KCSH_TESTS), 1)
